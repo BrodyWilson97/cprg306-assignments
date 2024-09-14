5b95b361f2feb1d68ca49123eafe6a436e53ffb4
BrodyWilson97 <153255880+BrodyWilson97@users.noreply.github.com>
2024-09-14 09:52:18 -0600

    Week 2 Assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info.js";
+
+
+export default function Page() {
+    return (
+    <main>
+        <StudentInfo />
+    </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+export default function StudentInfo() {
+    return (
+        <main>
+            <h1>Brody Wilson</h1>
+
+            <ul>
+                <li>
+                    <a href = "https://github.com/BrodyWilson97">BrodyWilson97</a>
+                </li>
+            </ul>
+        </main>
+    );
+}
+
+
